7ced43157ee0ee30d60722231bafc7a7f6dbd0fe
Adedeji Adepetun <adedeji.adepetun@postgrad.manchester.ac.uk>
2017-11-20 15:31:34 +0000

    Added the last column data and header to the result of the author_stats() function - Rui

--- src/comp62521/database/database.py
+++ src/comp62521/database/database.py
@@ -377,10 +377,11 @@ class Database:
         header, data = self.get_publications_by_author()
         header = ("Author", "Number of conference papers",
                   "Number of journals", "Number of books",
-                  "Number of book chapters", "Total publications", "First")
+                  "Number of book chapters", "Total publications", "First", "Last")
         for i in range(len(data)):
             first, last = self.first_last_author(data[i][0])
             data[i].append(first)
+            data[i].append(last)
         return header, data
 
 
