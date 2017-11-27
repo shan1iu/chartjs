5e6c356e930f03c2b68ccd11939168fd5cccd7e8
Adedeji Adepetun <adedeji.adepetun@postgrad.manchester.ac.uk>
2017-11-27 10:44:13 +0000

    created routing function for the author_statistics.html page

--- src/comp62521/views.py
+++ src/comp62521/views.py
@@ -117,6 +117,14 @@ def search_for_author():
     args["data"] = db.get_matching_authors(name)
     return render_template('author_search.html', args=args)
 
+@app.route("/authorstatistics/<name>")
+def show_author_statiostics(name):
+   dataset = app.config['DATASET']
+   db = app.config['DATABASE']
+   args = {"dataset":dataset, "name":name}
+   args["title"] = "Comprehensive Statistics for " + name
+   args["data"] = db.get_all_author_stats(name)
+   return render_template('author_statistics.html', args=args)
 
 @app.route("/statisticsdetails/<status>")
 def showPublicationSummary(status):
