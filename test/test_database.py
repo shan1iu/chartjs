5088c71c004558106670c57710606564d4ac6601
Adedeji Adepetun <adedeji.adepetun@postgrad.manchester.ac.uk>
2017-11-27 05:09:42 +0000

    Initial failing test for get_last_author_stat() - Cengiz

--- test/test_database.py
+++ test/test_database.py
@@ -229,6 +229,12 @@ class TestDatabase(unittest.TestCase):
         first = db.get_first_author_stat(pubs, 'Stefano Ceri')
         self.assertEqual(first, 4)
 
+    def test_get_last_author_stat(self):
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
+        last = db.get_last_author_stat(db.publications, 'Stefano Ceri')
+        self.assertEqual(last, 0)
+
     def test_get_all_author_stats(self):
         db = database.Database()
         self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
