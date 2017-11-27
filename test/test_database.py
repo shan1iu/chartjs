e91215d3337b16451c5deedc78f6c1dcdb1adbad
Adedeji Adepetun <adedeji.adepetun@postgrad.manchester.ac.uk>
2017-11-27 06:40:43 +0000

    Test for sort_author_group_by_name that sorts each group appopriately - Cengiz

--- test/test_database.py
+++ test/test_database.py
@@ -280,9 +280,7 @@ class TestDatabase(unittest.TestCase):
                                                  "Sam Brian", "Samuel Brian", "Alice Esam", 'Brian Esam', 'Alice Sam',
                                                  'Brian Sam', 'Alice Sammer', 'Brian Sammer', 'Alice Samming',
                                                  'Brian Samming'], 'sam')
-        print authors
-        self.assertEqual(authors, ['Alice Sam', 'Brian Sam', 'Alice Sammer', 'Alice Samming', 'Brian Sammer', 'Brian Samming', 'Sam Alice', 'Sam Brian', 'Samuel Alice', 'Samuel Brian', 'Alice Sam Brian', 'Brian Sam Alice', 'Alice Esam', 'Brian Esam']
-                         )
+        self.assertEqual(authors, ['Alice Sam', 'Brian Sam', 'Alice Sammer', 'Brian Sammer', 'Alice Samming', 'Brian Samming', 'Sam Alice', 'Sam Brian', 'Samuel Alice', 'Samuel Brian', 'Brian Sam Alice', 'Alice Sam Brian', 'Alice Esam', 'Brian Esam'])
 
     def test_sort_author_group_by_name(self):
         db = database.Database()
@@ -291,12 +289,11 @@ class TestDatabase(unittest.TestCase):
                                                  "Sam Brian", "Samuel Brian", "Alice Esam", 'Brian Esam', 'Alice Sam',
                                                  'Brian Sam', 'Alice Sammer', 'Brian Sammer', 'Alice Samming',
                                                  'Brian Samming'], 'sam')
-        authors = db.sort_author_group_by_name(authors)
+        authors = db.sort_author_group_by_name(authors, 2)
         self.assertEqual(authors,
-                         ['Alice Sam', 'Alice Sammer', 'Alice Samming', 'Alice Sam Brian', 'Alice Esam', 'Brian Sam',
-                          'Brian Sammer', 'Brian Samming', 'Brian Sam Alice', 'Brian Esam', 'Sam Alice', 'Sam Brian',
-                          'Samuel Alice', 'Samuel Brian']
-                         )
+                         ['Alice Sam Brian', 'Alice Esam', 'Alice Sam', 'Alice Sammer', 'Alice Samming',
+                          'Brian Sam Alice', 'Brian Esam', 'Brian Sam', 'Brian Sammer', 'Brian Samming', 'Sam Alice',
+                          'Sam Brian', 'Samuel Alice', 'Samuel Brian'])
 
     def test_get_network_data(self):
         db = database.Database()
